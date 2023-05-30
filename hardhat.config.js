a7e3f2092b5191cb034b3bc324455ef82501ef01
Anton Kravchenko <antonkravcenko@Antons-MacBook-Pro.local>
2023-05-30 11:58:40 +0200

    added deployment script in scripts

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,5 +1,7 @@
 require("@nomiclabs/hardhat-waffle");
-/** @type import('hardhat/config').HardhatUserConfig */
+require("@nomiclabs/hardhat-etherscan");
+require('dotenv').config();
+
 module.exports = {
   solidity: "0.8.18",
   settings: {
@@ -8,4 +10,13 @@ module.exports = {
       runs: 10,
     },
   },
+  networks: {
+    mumbai: {
+      url: process.env.RPC,
+      accounts: [`0x${process.env.PRIVATE_KEY}`]
+    },
+  },
+  etherscan: {
+    apiKey: process.env.ETHERSCAN_API_KEY
+  }
 };
